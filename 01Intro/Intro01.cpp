21c9967f1578cbf5abe05d36bd8e130944b48377
espgo <espgo@MIAMI>
2025-02-24 18:34:55 +0200

    Add project files.

---
+++ 01Intro/Intro01.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "\t\tGoing to school\n\t\tThinking about what I’m going to do in class\n\t\tMight just go to class and find out\n\t\tSchool is sometimes bad and good\n";
+	cout << "\t\tAlways focused when I’m doing homework\n\t\tMy friend is like a turtle trying to make it to school\n\t\tWhen I don’t do my homework I feel like a fool\n";
+	cout << "\t\tMy friends study like it’s a tool\n\t\tMy class likes the rules in school\n\t\tMy friend comes to school with some jewels\n\t\t\t\t\t\t\tWas writen by annonimus\n";
+	cout << "\tEvery\n\t\thunter\n\t\t\twants\n\t\t\t\tto\n\t\t\t\t\tknow\n\t\t\t\t\t\twhere\n\t\t\t\t\t\t\tthe\n\t\t\t\t\t\t\t\tpheasant\n\t\t\t\t\t\t\t\t\tis.\n\n";
+	cout << "\t\t\t\tsellin puppies\n\t\t\t\tkind: rottweiler\n\t\t\t\tphone 222-22-22\n\t\t\t\t222\t\|222\|\n\t\t\t\t22\t\|22\|\n\t\t\t\t22\t\|22\|";
+	
+		
+		
+		
+		
+		
+		
+
+
+}
