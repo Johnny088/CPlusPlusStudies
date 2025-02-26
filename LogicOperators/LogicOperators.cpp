b3f4ab9446bd2c0ecf3af899b6e56c3e80862796
Johnny <esp25goodlife@gmail.com>
2025-02-26 18:53:50 +0200

    ClassWork of February 26

---
+++ LogicOperators/LogicOperators.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "1. Given the sides of a triangle.Determine what type of triangle it is(equilateral, isosceles, right - angled).\n";
+	cout << " Enter the value of triangle sides: \n";
+	float a;
+	cin >> a;
+	float b;
+	cin >> b;
+	float c;
+	cin >> c;
+	if (a == b && b == c)
+	{
+		cout << "this triangle has is rivn";
+	}
+	else if (a == b || b == c || a == c)
+	{
+		cout << "the triangle is: rivnobedreniy";
+
+	}
+	else if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == b * b + a * a))
+	{
+		cout << "the triangle is primocutniy";
+	}
+	
+
+
+}
