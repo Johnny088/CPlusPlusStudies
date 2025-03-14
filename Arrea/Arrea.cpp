a5cc5c520b2048c88e0711fbddb37b38331884ef
Johnny <esp25goodlife@gmail.com>
2025-03-14 13:42:58 +0200

    Homework is done

--- Arrea/Arrea.cpp
+++ Arrea/Arrea.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	cout << "creating arrea for 10 elements\n";
+	/*cout << "creating arrea for 10 elements\n";
 	unsigned long long multy = 1;
 	const int size = 10;
 	int array1[size];
@@ -84,11 +84,179 @@ int main()
 		array5[i] *= -1;
 		cout << array5[i] << " ";
 
+	}*/
+	cout << endl << "Homework\n \tTask1: need to show n of number[1], 2n of number[2] 3n of number[3]\n";
+	const int SIZE = 3;
+	int array6[SIZE] = { 1,2,3 };
+	int n;
+	cout << "Enter the variable [n]\n";
+	cout << "Vatiable 'n' is : "; 
+	cin >> n;
+	int n2 = n;
+	/*for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < n; j++)
+		{
+			if (j == 0)
+			{
+				cout << array6[j];
+			}
+		}*/
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << i;
+		}
+		n +=n2;
+		cout << endl;
 	}
+	cout << "task2. \n\t show the square\n";
+	int square_size;
+	cout << "the size of the square is: ";
+	cin >> square_size;
 
+	for (int i = 0; i < square_size; i++)
+	{
+		for (int j = 0; j < square_size; j++)
+		{
+			if (i == j || i + j == square_size - 1)
+			{
+				cout << 0 << " ";
+			}
+			else
+			{
+				cout << 1 << " ";
+			}
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "task3. \n\tNeed to show numbers 10 times '10', 9 times '9' and so on until the number '1'\n";
+	int mount = 10;
+	int sum = 0;
+	for (int i = 10; i >= 1; i--)
+	{
+		for (int j = 1; j <=mount; j++)
+		{
+			cout << i;
+			sum += i;
+		}
+		mount--;
+		cout << endl;
+	}
+	cout << endl << "the sum of the numbers is: " << sum << endl;
+	cout << "task4. \n\t\Need to show 15 mount of strings and each even string has 8 numbers, and value of the numbers has to be equal to the number of string\n\t\tOtherwise mount of numbers are 10\n";
+	for (int i = 0; i < 15; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				cout << i+1;
+			}
+			cout << endl;
 
-
-	
+		}
 		
+		else
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				cout << i+1;
+			}
+			cout << endl;
+
+		}
+	}
+	cout << "task5. \n\t\Need to show 30 strings. Odd strings have numbers from 1 to the number of the string, even strings have 5 '1' \n\t\t\n";
+	for (int i = 1; i <= 30; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				cout << 1;
+			}
+			cout << endl;
+
+		}
+
+		else
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				cout << j << " ";
+			}
+			cout << endl;
+
+		}
+	}
+	cout << "task6. \n\t\Need to show the mulriplicatin table from 1 to 10\n";
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+	cout << "task7. \n\t\Need to find each number from A to B wich milpiples of 12.\n";
+	int A;
+	int B;
+	int C;
+	cout << "Enter the first number of range: ";
+	cin >> A;
+	C = A;
+	cout << "Enter the second number of range: ";
+	cin >> B;
+	if (A > B)
+	{
+		A = B;
+		B = C;
+
+	}
+	for (int i = A; i <= B; i++)
+	{
+		if (i % 12 == 0)
+		{
+			cout << i << " ";
+		}
+		
+	}
+	cout << endl;
+	cout << "task8. \n\t\Given salary of the year, and the range. need determine min&max salary of this range.\n";
+	const int size = 12;
+	float array10[size];
+	int month_start;
+	int month_end;
+	for (int i = 1; i <= size; i++)
+	{
+		cout << "Enter the salary of the " << i << " month - ";
+		cin >> array10[i-1];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << "The salary of the month " << i << " is: " << array10[i] << endl;
+	}
+	cout << endl << "enter the range of months to know min&max salary of that time\n";
+	cout << "Enter the 1 month: ";
+	cin >> month_start;
+	cout << "Enter the 1 month: ";
+	cin >> month_end;
+	int min = month_start;
+	int max = month_start;
+	for (int i = month_start; i <= month_end; i++)
+	{
+		if (array10[i]>max)
+		{
+			max = i;
+		}
+		if (array10[i] < min)
+		{
+			min = i;
+		}
+	}
+	cout << "Max of this time is " << max << endl;
+	cout << "Min of this time is " << min << endl;
 
 }
