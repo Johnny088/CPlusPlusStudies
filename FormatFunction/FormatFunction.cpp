dbb8c931cd4bcdfef0124677d8bae469f4f1f84c
Johnny <esp25goodlife@gmail.com>
2025-03-25 15:46:55 +0200

    updated

--- FormatFunction/FormatFunction.cpp
+++ FormatFunction/FormatFunction.cpp
@@ -2,7 +2,37 @@
 #include <iomanip>
 #include <Windows.h>
 using namespace std;
+void stringlen(char text[], int size)
+{
+    int len = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (text[i] != '\0')
+            len++;
+        else
+            break;
 
+
+    }
+    cout << "this text has " << len << " symbols include spaces\n";
+}
+char DelSymbol(char text[], char text2[], int size)
+{
+    char symbol;
+    cout << "enter the simbol we need to delete" << endl;
+    cin >> symbol;
+    for (int i = 0; i < size; i++)
+    {
+        char temp;
+        if (text[i] != symbol)
+        {
+            temp = text[i];
+            strcat(text2, text[i]);
+        }
+
+
+    }
+}
 int main()
 {
     /*for (int i = 0; i < 255; i++)
@@ -10,12 +40,12 @@ int main()
         cout << "code = " << i << " char =  " << (char)i << endl;
     }*/
     char line[255];
-    cout << "Enter line:\n";
-    cin.getline(line, 255);
-    cout << line;
+    /*cout << "Enter line:\n";
+    cin.getline(line, 255);*/
+    /*cout << line;*/
     int countA = 0;
     int countO = 0;
-    for (int i = 0; i < 255; i++)
+    /*for (int i = 0; i < 255; i++)
     {
         if (line[i] == 'a' || line[i] == 'A')
         {
@@ -58,6 +88,35 @@ int main()
     cout << "The space in line are " << CountSpace << " times\n";
     cout << "Task3. ";
     char text[255] = "Lorem Ipsum is simply DUMMY text OF the printing AND typesetting industry. LOREM Ipsum has been THE industry's standard dummy";
+    int size = strlen(text);
     cout << text << endl;
-    cout << "THe second way: \n";
+    cout << "The changed: \n";
+    for (int i = 0; i < size; i++)
+    {
+        if (islower(text[i]))
+        {
+            text[i] = toupper(text[i]);
+            cout << text[i];
+        }
+        else
+        {
+            text[i] = tolower(text[i]);
+            cout << text[i];
+
+        }
+            
+
+    }
+    cout << endl;*/
+    cout << "task4. Need to make a function which return the lenght of the string without strlen\n";
+    char text2[255] = "Lorem Ipsum is simply DUMMY text OF the printing AND typesetting industry.";
+    cout << text2 << endl;
+    stringlen(text2, 255);
+    int i = strlen(text2);
+    cout << "strlen(text2) =  " << i << endl;
+    cout << "task5. Given a string, need to delete  \n";
+    char text3[255] = "Lorem Ipsum is simply DUMMY text OF the printing AND typesetting industry.";
+    char text4[255];
+    DelSymbol(text3, text4, 255);
+
 }
