204f73fa9ce8a694508fb73a16e3075dbeb78d6d
Johnny <esp25goodlife@gmail.com>
2025-04-06 18:19:14 +0200

    I have a lot of questions about this homework

--- StructuresPart2/StructuresPart2.cpp
+++ StructuresPart2/StructuresPart2.cpp
@@ -53,9 +53,9 @@ void EditBook(library book[])
     cin >> book[index-1].genre;
     
 }
-void menu()
+void menu(library book[])
 {
-    library book[size];
+    /*library book[size];
 
     book[0] = { "The great getsby", "Scott Finzgerald", "Novel", "Charles Scribner's Sons"};
     book[1] = { "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "fantasy", "bloombury"};
@@ -66,7 +66,7 @@ void menu()
     book[6] = { "Brave ne world", "Aldous Huxley", "science fiction", "chatto"};
     book[7] = { "The picture of Dorifn Gray", "Oscar Wilde", "gotgic Fiction", "Lippincott's Monthly Magazine" };
     book[8] = { "One hundred years oh Solitude", "Gabriel Garcia Marquez", "fiction", "Editorial Sudamericana" };
-    book[9] = { "The dairy of a young girl", "Anne Frank", "Biografy", "contact Publishing" };
+    book[9] = { "The dairy of a young girl", "Anne Frank", "Biografy", "contact Publishing" };*/
     int choice;
     
     
@@ -107,6 +107,18 @@ void menu()
 }
 int main()
 {
-    menu();
+    library book[size];
+    book[0] = { "The great getsby", "Scott Finzgerald", "Novel", "Charles Scribner's Sons" };
+    book[1] = { "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "fantasy", "bloombury" };
+    book[2] = { "The catcher in the river", "J.D. Salinger", "fiction", "Litle" };
+    book[3] = { "The lord of the ring", "J.R.R. Tolkien", "fantasy", "Alien" };
+    book[4] = { "The Hobbit", "J.R.R. Tolkien", "fantasy", "alien" };
+    book[5] = { "The alchemist", "J.D. Salinger", "fiction", "little" };
+    book[6] = { "Brave ne world", "Aldous Huxley", "science fiction", "chatto" };
+    book[7] = { "The picture of Dorifn Gray", "Oscar Wilde", "gotgic Fiction", "Lippincott's Monthly Magazine" };
+    book[8] = { "One hundred years oh Solitude", "Gabriel Garcia Marquez", "fiction", "Editorial Sudamericana" };
+    book[9] = { "The dairy of a young girl", "Anne Frank", "Biografy", "contact Publishing" };
+
+    menu(book);
     
 }
