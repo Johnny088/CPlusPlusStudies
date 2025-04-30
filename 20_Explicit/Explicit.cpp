e2bda47085aa7e629daaafa8eabb593d0db4b004
Johnny <esp25goodlife@gmail.com>
2025-04-30 11:35:52 +0200

    everithing exept addin to the array

--- 20_Explicit/Explicit.cpp
+++ 20_Explicit/Explicit.cpp
@@ -50,6 +50,86 @@ public:
 			arr[i] = rand() % 80 + 10;
 		}
 	}
+	void SetNull()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = 0;
+		}
+	}
+	void SetByUser()
+	{
+		int temp;
+		for (int i = 0; i < size; i++)
+		{
+			SetColor(6);
+			cout << "enter the value of the item number: " << i + 1 << " "; cin >> temp;
+			arr[i] = temp;
+		}
+		cout << "\n";
+	}
+	void print() const
+	{
+		SetColor(6);
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+	}
+	/*void add()
+	{
+		int value;
+		cout << "Enter the value: "; cin >> value;
+		size++;
+		Array* temp = new Array[size];
+		for (int i = 0; i < size-1; i++)
+		{
+			temp[i] = arr[i];
+		}
+		temp[size - 1] = value;
+		if (arr != nullptr)
+		{
+			delete[]arr;
+		}
+		arr = temp;
+	}*/
+	void Min()
+	{
+		int min = arr[0];
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i] < min)
+			{
+				min = arr[i];
+			}
+		}
+		SetColor(6);
+		cout << "min of the Array is: " << min << "\n";
+	}
+	void Max()
+	{
+		int min = arr[0];
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i] > min)
+			{
+				min = arr[i];
+			}
+		}
+		SetColor(6);
+		cout << "max of the Array is: " << min << "\n";
+	}
+	void average()
+	{
+		int temp =0;
+		for (int i = 0; i < size; i++)
+		{
+			temp += arr[i];
+		}
+		SetColor(6);
+		cout << "The average of the numbers is: " << temp/size <<"\n";
+	}
 	/*~Array()
 	{
 		if (arr != nullptr)
@@ -60,21 +140,43 @@ public:
 };
 void menu()
 {
-	Array v1;
+	Array v1(10);
 	int Menu, Size;
 	do
 	{
-
-		cout << "Main menu: \n1 - to make an array \n2 - to fill out the array by random numbers";
+		SetColor(11);
+		cout << "\tMain menu: \n1 - to fill out the array by random numbers \n2 - to turn all numbers into '0' \n3 - to set up the numbers by user\n";
+		cout << "4 - to add element \n5 - min of the numbers \6 -  maximum of the elements \n 7 - the average of the elements \n8 - print \n";  SetColor(7);
 		cin >> Menu;
 		switch (Menu)
 		{
 		case 1:
-			cin >> Size;
-			v1(Size);
+			v1.SetRandoms();
+			break;
+		case 2:
+			v1.SetNull();
+			break;
+		case 3:
+			v1.SetByUser();
+			break;
+		case 4:
+			SetColor(12);
+			cout << "doesn't work\n";
+			break;
+		case 5:
+			v1.Min();
+			break;
+		case 6:
+			v1.Max();
+			break;
+		case 7:
+			v1.average();
+			break;
+		case 9:
+			v1.print();
 			break;
 		case 0:
-			cout << "have a nice day";
+			SetColor(6); cout << "have a nice day\n"; SetColor(7);
 			break;
 		default:
 			SetColor(12); cout << "your choice is incorrect\n"; SetColor(7);
