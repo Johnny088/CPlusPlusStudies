6cde8391927bd93301f43318b6cb490daca6d63e
Johnny <esp25goodlife@gmail.com>
2025-05-20 11:14:31 +0200

    done

---
+++ 27_QueueWithPriority/27_QueueWithPriority.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+
+//struct Element
+//{
+//	string value;
+//	int priority;
+//}; 
+//ostream& operator<<(ostream& out, const Element& other)
+//{
+//	out << "Value : " << other.value << ". Priority : " << other.priority ;
+//	return out;
+//}
+template<typename T>
+class QueueWithPriority
+{
+private:
+	
+	struct Element
+	{
+		T Document;
+		int priority;
+		void Print()
+		{
+			cout << "Value : " << Document << ". Priority : " << priority;
+		}
+	};
+
+	Element* arr;
+	int size;
+public:
+	QueueWithPriority() :arr(nullptr), size(0) {}
+	~QueueWithPriority()
+	{
+		if (arr != nullptr)delete[]arr;
+	}
+	void Enqueue(T value, int priority)
+	{
+		int index = 0;
+		while (index < size && arr[index].priority < priority)
+		{
+			index++;
+		}
+		size++;
+		Element* temp = new Element[size];
+		for (int i = 0; i < index; i++)
+		{
+			temp[i] = arr[i];
+		}
+		temp[index] = Element{ value, priority };
+		for (int i = index; i < size - 1; i++)
+		{
+			temp[i + 1] = arr[i];
+		}
+		if (arr != nullptr)delete[]arr;
+		arr = temp;
+	}
+	bool IsEmpty()
+	{
+		return size == 0;
+	}
+	Element Dequeue()
+	{
+		if (!IsEmpty())
+		{
+			Element first = arr[0];
+			for (int i = 0; i < size - 1; i++)
+			{
+				arr[i] = arr[i + 1];
+			}
+			size--;
+			return first;
+		}
+		return Element();
+	}
+	void Print()
+	{
+		cout << "-------------- Queue ---------------" << endl;
+		for (int i = 0; i < size; i++)
+		{
+			arr[i].Print();
+			cout << endl;
+		}
+		cout << "_____________________________________" << endl;
+	}
+};
+
+int main()
+{
+	QueueWithPriority <string> q;
+
+	/*QueueWithPriority::Element el = { "A",7 };
+	q.Enqueue(el);
+
+	q.Enqueue(QueueWithPriority::Element{ "B",2 });
+	q.Enqueue(QueueWithPriority::Element{ "B",8 });
+	q.Enqueue(QueueWithPriority::Element{ "W",2 });
+	q.Enqueue(QueueWithPriority::Element{ "C",3 });
+	q.Enqueue(QueueWithPriority::Element{ "G",5 });
+	q.Enqueue(QueueWithPriority::Element{ "K",1 });*/
+
+	q.Enqueue("A", 7);
+
+	q.Enqueue("B", 2);
+	q.Enqueue("B", 8);
+	q.Enqueue("W", 2);
+	q.Enqueue("C", 3);
+	q.Enqueue("G", 5);
+	q.Enqueue("K", 1);
+	q.Print();
+
+	cout << "-----------------------------" << endl;
+	q.Enqueue("D", 4);
+	q.Print();
+	cout << "-----------------------------" << endl;
+	while (!q.IsEmpty())
+	{
+		q.Dequeue().Print();
+		cout << endl;
+	}
+
+
+
+
+}
